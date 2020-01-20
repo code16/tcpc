4e208ad608ac540b52b1e8ed1d9f1926602183b2
Philippe Lonchampt <philippelonchampt@gmail.com>
2020-01-20 17:50:23 +0100

    Homepage

---
+++ source/index.blade.php
@@ -0,0 +1,6 @@
+@extends('_layouts.master')
+@section('body')
+    <div class="text-center mt-4 mb-4">
+        <img class="tcpc mw-100" src="assets/images/tcpc.gif" alt="TCPC">
+    </div>
+@endsection
